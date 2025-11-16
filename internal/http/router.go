be3facb576c36f563a2c63b4dcce68e62f998a1c
rafly-ananda <anandarafly15@gmail.com>
2025-11-16 12:43:58 +0700

    chore(deployment): add nginx and docker-compose

--- internal/http/router.go
+++ internal/http/router.go
@@ -20,6 +20,7 @@ type Handlers struct {
 func NewRouter(h Handlers) *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
+	// TODO Add CORS later here
 	r.Use(cors.Default())
 
 	// Websocket Setup
